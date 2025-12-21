1b42a62512fd74259dc2ba8866ecb7dd00f827b4
bala-amypo <bala@amypo.in>
2025-12-21 18:49:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/PredictionResponse.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PredictionResponse {
+    private Long stockRecordId;
+    private LocalDate predictedRestockDate;
+    private String message;
+}
