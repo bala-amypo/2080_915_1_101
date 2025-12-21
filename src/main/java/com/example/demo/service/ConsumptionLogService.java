77263eed92b62bdd1bdbddbcb73b90c7c856711c
bala-amypo <bala@amypo.in>
2025-12-21 16:51:25 +0000

    User commit

--- src/main/java/com/example/demo/service/ConsumptionLogService.java
+++ src/main/java/com/example/demo/service/ConsumptionLogService.java
@@ -7,7 +7,9 @@ import java.util.List;
 public interface ConsumptionLogService {
 
     ConsumptionLog logConsumption(Long stockRecordId, ConsumptionLog log);
-
+    
+    ConsumptionLog getLog(String id);
+     
     List<ConsumptionLog> getLogsByStockRecord(Long stockRecordId);
 
     ConsumptionLog getLog(Long id);
