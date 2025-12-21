7c4ca38b1a85a5058b17ac356cbea14039e5f4dc
bala-amypo <bala@amypo.in>
2025-12-21 07:42:44 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> { // [cite: 126]
-    Optional<User> findByEmail(String email); // [cite: 127]
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
