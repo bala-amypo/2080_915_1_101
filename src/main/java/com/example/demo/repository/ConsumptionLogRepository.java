ee89f8879085d11d93ae8ed7dcf0142d9a1d51ba
bala-amypo <bala@amypo.in>
2025-12-22 09:05:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/ConsumptionLogRepository.java
+++ src/main/java/com/example/demo/repository/ConsumptionLogRepository.java
@@ -3,20 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.model.ConsumptionLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ConsumptionLogRepository extends JpaRepository<ConsumptionLog, Long> {
 
     List<ConsumptionLog> findByStockRecordId(Long stockRecordId);
-
-    /* ===== METHODS REQUIRED BY TESTS ===== */
-
-    List<ConsumptionLog> findByStockRecordIdAndConsumedDateBetween(
-            Long stockRecordId,
-            LocalDate startDate,
-            LocalDate endDate
-    );
-
-    List<ConsumptionLog> findByStockRecordIdOrderByConsumedDateDesc(Long stockRecordId);
 }
